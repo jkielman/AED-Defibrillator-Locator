913a76cd1e3c27e6faf673f4caea7e4c862d0d39
Jeff Kielman <info@jeffkielman.com>
2018-07-25 21:30:10 -0400

    Added getLocation

--- src/index.js
+++ src/index.js
@@ -8,26 +8,70 @@ import data from './data/data.json';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
 
-const App = () => (
-  <Row>
-  <Col xs={12}>
-    <Row center="xs">
-      <Col xs={12} sm={6} >
 
-      <h1>Closet Defibrillator</h1>
 
-        {
-          data.map(function(defib){
-            return <div>{defib.Facility_Name}{defib.Street_Address}{defib.Device_Counts}</div>;
-          })
-        }
 
-      </Col>
-    </Row>
-  </Col>
-  </Row>
-)
 
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      latitude: '',
+      longitude: '',
+    }
+    this.getMyLocation = this.getMyLocation.bind(this)
+  }
+
+  componentDidMount() {
+    this.getMyLocation()
+  }
+
+  getMyLocation() {
+    const location = window.navigator && window.navigator.geolocation
+
+    if (location) {
+      location.getCurrentPosition((position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        })
+      }, (error) => {
+        this.setState({ latitude: 'err-latitude', longitude: 'err-longitude' })
+      })
+    }
+
+  }
+
+  render() {
+    const { latitude, longitude } = this.state
+
+    return (
+      <div>
+        {/*<input type="text" value={latitude} />
+        <input type="text" value={longitude} />*/}
+        <Row>
+          <Col xs={12}>
+            <Row center="xs">
+              <Col xs={12} >
+              <h1>Your Location</h1>
+              <h1>Lat: {latitude} / Lon: {longitude} </h1>
+              </Col>
+              <Col xs={12} >
+              <h1>Nearest Locations</h1>
+              {
+                data.map((defib, index ) => {
+                  return <div key={index}>{defib.Facility_Name}{defib.Street_Address}{defib.Device_Counts}</div>
+                })
+              }
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </div>
+
+    )
+  }
+}
 
 ReactDOM.render(
   <App/>,
